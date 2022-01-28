415cf0ae40ba9e735a31a686a08e8e60193a0865
syokaturyou <teamsumagaku2@gmail.com>
2022-01-28 09:28:04 +0000

    1/28 2

--- memo.rb
+++ memo.rb
@@ -1,7 +1,12 @@
 require "csv"
+
+puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+
+# csvファイル読み込み例
+CSV.foreach("data.csv") do |row|
+    p row
+end
  
-    puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+    # memo_type = gets.to_s
  
-    memo_type = gets.to_s
- 
-    //続きを書いていきましょう！！(ifで条件分岐)
+    # //続きを書いていきましょう！！(ifで条件分岐)
