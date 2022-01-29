6b315d6e2ab02cf69d624ef77d2724d65855e452
syokaturyou <teamsumagaku2@gmail.com>
2022-01-29 06:50:24 +0000

    メモアプリ完成

--- memo.rb
+++ memo.rb
@@ -1,27 +1,38 @@
 require "csv"
-
 puts "1(新規でメモを作成) 2(既存のメモ編集する)"
-
-## ユーザーのキーワード入力を待機
-memo = gets.to_i
+memo_type = gets.to_i
     
-if memo == 1
-   puts "1を選択"
-elsif memo == 2
-   puts "2を選択"
+if memo_type == 1
+   puts "拡張子を除いたファイル名を入力してください"
+   filename = gets.to_s
+   
+   if File.exist?(filename + '.csv')
+    puts '既に同名ファイルが存在します。'
+   else
+    puts "メモしたい内容を記入してください"
+    puts "完了したら Ctrl + D を押します"
+    naiyo = STDIN.read
+    File.write(filename + ".csv",naiyo)
+   end
+
+elsif memo_type == 2
+   puts "拡張子を除いたファイル名を入力してください"
+   filename = gets.to_s
+   
+   if File.exist?(filename + '.csv')
+     puts "csvファイルの現内容を表示"
+     puts "----------------------"
+     f = File.open(filename + ".csv")
+     puts "#{f.read}"
+     puts "----------------------"
+     puts "更新内容を記入してください"
+     puts "完了したら Ctrl + D を押します"
+     naiyo = STDIN.read
+     File.write(filename + ".csv",naiyo)
+   else
+     puts '該当ファイルは存在しません。'
+   end
+   
 else
    puts "1か2を入力してください"
 end
-
-# csvファイル読み込み例
-# CSV.foreach("data.csv") do |row|
-#     p row
-# end
-
-# csvファイル読み込み例
-CSV.open('test.csv','w') do |test|
- test << ["A","B","C"]
- test << ["milk","coffee","water"]
-end
- 
-    # //続きを書いていきましょう！！(ifで条件分岐)
